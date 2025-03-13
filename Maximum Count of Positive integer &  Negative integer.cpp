e394a432c1b8f397e0b051e27414553b2de2ed89
ASHMIT BHATIA <152895406+ashmitbhatia520@users.noreply.github.com>
2025-03-13 22:09:25 +0530

    Create Maximum Count of Positive integer &  Negative integer.cpp

---
+++ Maximum Count of Positive integer &  Negative integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                cnt1++;
+            }
+            if(nums[i]>0){
+                cnt2++;
+            }
+        }
+        return max(cnt1,cnt2);
+    }
+};
